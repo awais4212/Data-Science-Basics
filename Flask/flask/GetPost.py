7933536474dbd3689361b71c6950c0211c2d508e
Syed Muhammad Awais Hashmi <160838760+awais4212@users.noreply.github.com>
2026-08-01 20:41:16 +0500

    Added Some File

--- Flask/flask/GetPost.py
+++ Flask/flask/GetPost.py
@@ -15,7 +15,12 @@ def form():
         return f"Hello {name}!"
     return render_template('form.html')
 
-
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method=='POST':
+            name = request.form['name']
+            return f"Hello {name}!, You have submitted the form thank you"
+    return render_template('form.html')
 
 if __name__=='__main__':
     app.run(debug=True)
